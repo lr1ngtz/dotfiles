f06f96208a440a874a7b61c40d9510d6d47d5232
lr1ngtz <sko0hlol@gmail.com>
2025-04-24 20:03:44 +0200

    Update init.el

--- init.el
+++ init.el
@@ -3,16 +3,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(misterioso))
- '(custom-safe-themes '(default))
+ '(custom-enabled-themes '(gruvbox-dark-medium misterioso))
+ '(custom-safe-themes
+   '("5a0ddbd75929d24f5ef34944d78789c6c3421aa943c15218bac791c199fc897d"
+     default))
  '(package-selected-packages
-   '(consult git-gutter-fringe marginalia orderless pbcopy vertico)))
+   '(consult git-gutter-fringe gruvbox-theme marginalia orderless pbcopy
+             vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; load gruvbox-theme
+(use-package gruvbox-theme
+  :ensure t
+  :config
+  (load-theme 'gruvbox-dark-medium t)) ; Or gruvbox-light-soft, etc.
 
 ;; ensure emacs can find executables installed by homebrew on intel macs
 (add-to-list 'exec-path "/usr/local/bin")
@@ -26,10 +35,10 @@
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
+;;; load lisp
 ;; custom lisp directory to load path
 (add-to-list 'load-path(expand-file-name "~/.emacs.d/lisp"))
 
-;; load lisp
 ;; odin syntax highlighting
 (load "odin-mode")
 
@@ -79,7 +88,7 @@
 
 (global-set-key (kbd "C-x ;") 'comment-line)
 
-;; enable vertico, consult, orderless, and marginalia
+;;; enable vertico, consult, orderless, and marginalia
 (vertico-mode 1)
 (marginalia-mode 1)
 (setq completion-styles '(orderless basic)
@@ -90,18 +99,25 @@
 (setq project-vc-ignores '("*/__pycache__/*"))
 
 (global-set-key (kbd "M-g g") 'consult-goto-line)
-(global-set-key (kbd "C-c f") 'consult-find)
+(global-set-key (kbd "C-c f") 'consult-fd)
 (global-set-key (kbd "C-c r") 'consult-ripgrep)
 
-;; git-gutter
+;;; git-gutter
 (require 'git-gutter)
 (global-git-gutter-mode t)
+
+(defun gruvbox-git-gutter-faces ()
+  (custom-set-faces
+   `(git-gutter:added ((t (:foreground "#b8bb26" :background "#32302f"))))
+   `(git-gutter:deleted ((t (:foreground "#fb4934" :background "#32302f"))))
+   `(git-gutter:modified ((t (:foreground "#fabd2f" :background "#32302f"))))))
+(add-hook 'after-init-hook #'gruvbox-git-gutter-faces)
 
 (global-set-key (kbd "C-c g h") 'git-gutter:popup-hunk)
 (global-set-key (kbd "C-c g n") 'git-gutter:next-hunk)
 (global-set-key (kbd "C-c g p") 'git-gutter:previous-hunk)
 
-;; vc-diff
+;;; vc-diff
 (global-set-key (kbd "C-c d") 'vc-diff)
 
 (setq scroll-margin 5)
