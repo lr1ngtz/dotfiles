e83006a504ee358e4adfbc9ab8bdbec69cdbaf4f
lr1ngtz <sko0hlol@gmail.com>
2025-07-11 21:58:11 +0200

    Update emacs config, ghostty, alacritty

--- emacs.custom.el
+++ emacs.custom.el
@@ -3,13 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruvbox-dark-medium misterioso))
+ '(custom-enabled-themes '(catppuccin))
  '(custom-safe-themes
-   '("5a0ddbd75929d24f5ef34944d78789c6c3421aa943c15218bac791c199fc897d"
+   '("276228257774fa4811da55346b1e34130edb068898565ca07c2d83cfb67eb70a"
+     "8363207a952efb78e917230f5a4d3326b2916c63237c1f61d7e5fe07def8d378"
+     "75b371fce3c9e6b1482ba10c883e2fb813f2cc1c88be0b8a1099773eb78a7176"
+     "5aedf993c7220cbbe66a410334239521d8ba91e1815f6ebde59cecc2355d7757"
+     "18a1d83b4e16993189749494d75e6adb0e15452c80c431aca4a867bcc8890ca9"
+     "d5fd482fcb0fe42e849caba275a01d4925e422963d1cd165565b31d3f4189c87"
+     "51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3"
+     "5a0ddbd75929d24f5ef34944d78789c6c3421aa943c15218bac791c199fc897d"
      default))
  '(package-selected-packages
-   '(consult git-gutter-fringe gruvbox-theme marginalia orderless pbcopy
-             pyvenv vertico)))
+   '(consult eglot git-gutter-fringe go-mode gruvbox-theme lsp-mode
+             marginalia orderless pbcopy treesit-auto vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -8,10 +8,15 @@
 (setq-default standard-indent 4)
 
 ;;highlight trailing whitespace
+;; (global-whitespace-mode 1)
+;; (setq whitespace-style '(face trailing tabs spaces empty))
+;; (set-face-background 'whitespace-trailing "#cc0000") ; red background
+;; (set-face-foreground 'whitespace-trailing nil)
 (global-whitespace-mode 1)
-(setq whitespace-style '(face trailing tabs spaces empty))
+(setq whitespace-style '(face trailing))
 (set-face-background 'whitespace-trailing "#cc0000") ; red background
 (set-face-foreground 'whitespace-trailing nil)
+
 
 ;; enable c-x c-l to downcase selected region
 (put 'downcase-region 'disabled nil)
@@ -27,7 +32,7 @@
 (global-set-key (kbd "C-x ;") 'comment-line)
 
 ;;; load gruvbox-theme
-(require 'gruvbox-theme)
+;; (require 'gruvbox-theme)
 
 ;; ensure emacs can find executables installed by homebrew on intel macs
 (add-to-list 'exec-path "/usr/local/bin")
@@ -112,7 +117,6 @@
     (unless (file-directory-p base-path)
       (error "Odin directory %s does not exist" base-path))
     (consult-ripgrep base-path (concat search-term " -- core vendor base -i"))))
-
 ;; bind to c-c o
 (global-set-key (kbd "C-c o") #'odin-source-search)
 
@@ -152,3 +156,21 @@
                              default-directory)))
     (compile "odin build . -debug -out:build/main")))
 (global-set-key (kbd "C-c C-b") 'build-odin-debug-compile)
+
+
+;; Enable eglot for go-mode
+(require 'eglot)
+(add-hook 'go-mode-hook 'eglot-ensure)
+(add-to-list 'eglot-server-programs '(go-mode . ("gopls")))
+
+;; Optional: Enable eldoc for hover documentation
+(add-hook 'go-mode-hook 'eldoc-mode)
+
+(global-set-key (kbd "C-c h") 'eldoc)
+
+
+;; catppuccin theme
+(setq catppuccin-flavor 'latte) ; or 'latte, 'macchiato, or 'mocha
+(load-theme 'catppuccin t)
+(add-hook 'server-after-make-frame-hook #'catppuccin-reload) ; emacsclient
+
